baa0692eaf03af56bcc9ce6eb43ad58dc93757c6
sriramk <sriramk@clemson.edu>
2023-05-10 20:25:08 -0400

    updated 1D sys example

--- matlab/analytical1D/eigenfunctions_analytical1D.m
+++ matlab/analytical1D/eigenfunctions_analytical1D.m
@@ -8,15 +8,6 @@ set(0,'defaultfigurecolor',[1 1 1])
 % sys 1
 f = @(t, x) [-x(1,:)+x(1,:).^3];
 
-% sys 2
-% f = @(t, x) [lambda1*x(1,:); lambda2*(x(2,:)+x(1,:).^2)];
-% beta=-lambda2/(2*lambda1-lambda2);
-
-% get quiver
-% [X,Y] = meshgrid(Dom(1):0.5:Dom(2),Dom(1):0.5:Dom(2));
-% u = (lambda1.*X);
-% v = (lambda2.*Y-X.^2);
-
 % true eig functions
 x = sym('x', 'real');
 phi = @(x) x(1,:)/sqrt(1-x(1,:).^2);
@@ -30,21 +21,28 @@ w = W(:,1);
 g = @(x) w'*fn(x);
 
 %% Set up path Integral
-Dom = [-0.9 0.9]; ds = 0.005;
-% Dom = [-2 2]; ds = 0.05;
+dim=1;
+% Dom = [-0.9 0.9]; ds = 0.005;
+Dom = [-2 2]; ds = 0.001;
 grid = Dom(1):ds:Dom(2); %define grid where eigenfunction is well defined
+grid(grid==1)=nan;
+grid(grid==-1)=nan;
 x_0 = [grid(:)]; phi_est=[];
 
 %options = odeset('RelTol',1e-9,'AbsTol',1e-300,'events',@(t, x)offFrame(t, x, Dom(2)));
 options = odeset('RelTol',1e-9,'AbsTol',1e-300);
 
-t_span = [-200 0];
+t_span = [-10 0];
 parfor i = 1:length(x_0)
     % usde ode45 to test eig funs
     [t,x] = ode45(@(t,x)f(t,x),t_span,x_0(i,:),options);
 
     % for negative time
-    phi_est = [phi_est, w'*x_0(i,:)' - trapz(t,exp(-l*t).*g(x')')];
+%     phi_est = [phi_est, w'*x_0(i,:)' - trapz(t,exp(-l*t).*g(x')',dim)];
+    phi_est = [phi_est, trapz(t,exp(-l*t).*g(x')',dim)];
+
+    % for flow reversed
+    %phi_est = [phi_est, w'*x_0(i,:)' + trapz(t,exp(-l*t).*g(x')',dim)];
 end
 
 %% get true eig funs
@@ -52,16 +50,17 @@ Phi = zeros(size(grid));
 for i = 1:length(grid)
     Phi(i) = phi(grid(i)) ;
 end
-phi_est = reshape((phi_est),size(grid));
+% phi_est = reshape((phi_est),size(grid)-2);
 
 %% Eigenfunctions
 figure(1)
 subplot(1,2,1)
 plot(grid,Phi);
-title('true $\phi(x) = \frac{-x}{\sqrt{1-x^2}}$','Interpreter','latex')
+title('true $\phi(x) = \frac{x}{\sqrt{1-x^2}}$','Interpreter','latex')
 xlabel('$x$','interpreter','latex');
 ylabel('$\phi(x)$','interpreter','latex');
 set(gca,'fontsize',20)
+xlim([-0.9,0.9])
 axis square
 
 subplot(1,2,2)
@@ -70,6 +69,7 @@ title('estimated $\phi(x)$','Interpreter','latex')
 xlabel('$x$','interpreter','latex');
 ylabel('$\phi(x)$','interpreter','latex');
 set(gca,'fontsize',20)
+xlim([-0.9,0.9])
 axis square
 
 % subplot(1,3,3)
@@ -81,7 +81,6 @@ axis square
 % ylabel('$x_2$','interpreter','latex');
 % set(gca,'fontsize',20)
 % axis square
-
 
 %% helper functions
 function [value,isterminal,direction]=offFrame(~, Y, Dom)
